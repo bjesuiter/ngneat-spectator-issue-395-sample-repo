a9e094ec1a83cf357946bc134934cd13522578a7
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2021-02-11 09:12:59 +0100

    add external styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'spectator-issue395';
+	title = 'spectator-issue395';
 }
